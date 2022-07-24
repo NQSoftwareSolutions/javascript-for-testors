96bed649abd9bd0a572c93803db9a6ed05537ef1
Abdul Qayoom Jat <78290359+NQSoftwareSolutions@users.noreply.github.com>
2022-07-24 23:48:44 +0500

    Funtions and package json update

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Function is a peice of code which takes input as parameters, process then and return result as output
+// Function will be declare one time and it can be called any number of times.
+
+// declare a funtion
+
+function sum (a,b) {
+    return a + b
+}
+
+console.log("Sum is " + sum(5, 10))
+console.log("Sum is " + sum(5, 20))
