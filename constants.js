e2c6ffd8a6ae58a45b09392afe94dd4967d299d9
Abdul Qayoom Jat <78290359+NQSoftwareSolutions@users.noreply.github.com>
2022-07-24 10:45:50 +0500

    vars and consts update

---
+++ constants.js
@@ -0,0 +1,8 @@
+// Constants are special type of variables which can be only assign oone time
+// one assign then we wan not change value of constant
+
+const i = 10
+const j = 6
+
+console.log("Value of i is " + i)
+console.log("Value of j is " + j)
---
+++ variable.js
@@ -0,0 +1,8 @@
+// Variables are simple containers which can be assigned any value of one type
+// JS is dynamically typed language so we can assign multiple type opf values to one variable
+// Variable at a one time , it has only one type of value
+let i = 6 // good practice
+var j = 10 // old practice should be avoided
+
+console.log("Variable i is " + i)
+console.log("Variable j is " + j)
