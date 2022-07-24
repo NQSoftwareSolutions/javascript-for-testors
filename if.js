819cdbb48f8f31bf1d459a0be08dd1c8c3945c30
Abdul Qayoom Jat <78290359+NQSoftwareSolutions@users.noreply.github.com>
2022-07-24 10:44:55 +0500

    Javascript for testors

---
+++ if.js
@@ -0,0 +1,19 @@
+// we use if to evolute any condition
+
+let a = 9
+
+let isA10 = false
+
+if (a == 10){
+    isA10 = true
+}
+
+if (a < 10){
+    isA10 = false
+}
+
+if (a > 10){
+    isA10 = false
+}
+
+console.log("A is 10 " + isA10)
